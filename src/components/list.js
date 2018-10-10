81329a7cef4f7f79188b981414a3d009a64da34f
Kyle McV <kylemcv@gmail.com>
2018-10-10 15:13:32 -0700

    create forms

--- src/components/list.js
+++ src/components/list.js
@@ -3,13 +3,43 @@ import React, { Component } from 'react';
 class List extends Component {
 
     constructor(props){
-        super(props)
+        super(props);
+
+        this.state = {
+            item: "", 
+            items: []
+        };
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleInput(){
+        return (e => {
+            this.setState({ item: e.target.value });
+        });
+    }
+
+    handleSubmit(e){
+        e.preventDefault();
+        this.setState({ items: this.state.items.concat(this.state.item) });
+        this.setState({ item: "" });
+    }
+
+    handleLeft(){
+
     }
 
     render(){
         return(
             <div>
-                Hello from list!
+                <form>
+                    <input type="text" value={this.state.item} onChange={this.handleInput()} />
+                    <button onClick={this.handleSubmit} >X</button>
+                </form>
+                <ul>
+                    {this.state.items.map((el) => <li>
+                        {el}<button onClick={this.handleLeft}></button></li>)}
+                </ul>
             </div>
         );
     }
